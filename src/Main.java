17bbad3306375bb391772769b6e9556d8aa386f2
吴军 <1097057353@qq.com>
2020-05-06 16:09:45 +0800

    提交

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import com.leigq.WeatherService;
+import com.leigq.WeatherServiceService;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        WeatherServiceService factory = new WeatherServiceService();
+        WeatherService weatherServicePort = factory.getWeatherServicePort();
+        String cityname = weatherServicePort.getWeatherByCityname("重庆");
+        System.out.println(cityname);
     }
 }
